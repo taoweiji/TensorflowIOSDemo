b1c729ed01189d1cf5c76972207214cbd19c18ed
Wiki <taoweiji2008@qq.com>
2018-05-14 23:58:33 +0800

    Initial Commit

---
+++ TensorflowIOSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TensorflowIOSDemo
+//
+//  Created by Wiki on 2018/5/14.
+//  Copyright © 2018年 Wiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
